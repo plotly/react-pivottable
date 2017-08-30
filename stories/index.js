3d5c195378c9fa73bf018a35715f0fc0c7acefa1
Nicolas Kruchten <nicolas@datacratic.com>
2017-08-30 15:46:02 -0400

    fix up gh-pages

--- stories/index.js
+++ stories/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import { setAddon, storiesOf } from '@storybook/react';
-import JSXAddon from 'storybook-addon-jsx'
-setAddon(JSXAddon)
 
 import PivotTable from '../src/PivotTable'
 import { mps } from './SampleData'
 const dataSetHider = (dataset) => (injectRecord) => dataset.map(injectRecord);
 
 storiesOf('PivotTable', module)
-  .addWithJSX('Canadian MPs 2012 dataset', () => <PivotTable
+  .add('Canadian MPs 2012 dataset', () => <PivotTable
     cols={['Party']} rows={['Province']} data={dataSetHider(mps)} />
   );
 
