9e644122f76b877732059a954809767d2fba918e
Nicolas Kruchten <nicolas@datacratic.com>
2017-11-07 15:54:49 -0500

    website upgrades

--- examples/App.jsx
+++ examples/App.jsx
@@ -6,25 +6,100 @@ import createPlotlyComponent from 'react-plotly.js/factory';
 import createPlotlyRenderers from '../src/PlotlyRenderers';
 import PivotTableUI from '../src/PivotTableUI';
 import '../src/pivottable.css';
+import Dropzone from 'react-dropzone'
+import Papa from 'papaparse'
 
 const Plot = createPlotlyComponent(window.Plotly);
 
-export default class App extends React.Component {
+class PivotTableUISmartWrapper extends React.PureComponent {
     constructor(props) {
         super(props)
-        this.state = {
-            rows: ["Gender"], cols: ["Age Bin"],
-            rendererName: "Stacked Column Chart",
-            plotlyOptions: {width: 900, height: 500}
-        };
+        this.state = { pivotState: props };
+    }
+
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({pivotState: nextProps});
     }
 
     render() {
         return <PivotTableUI
-            data={mps}
             renderers={Object.assign({}, TableRenderers, createPlotlyRenderers(Plot))}
-            derivedAttributes={{"Age Bin": derivers.bin("Age", 10)}}
-            {...this.state} onChange={s => this.setState(s)}
+            {...this.state.pivotState} onChange={s => this.setState({pivotState: s})}
              />;
+    }
+}
+
+export default class App extends React.Component {
+    componentWillMount() {
+        this.setState({
+            mode: "demo",
+            filename: "Sample Dataset: Canadian Members of Parliament 2012",
+            pivotState: {
+                data: mps,
+                rows: ["Gender"], cols: ["Age"],
+                rendererName: "Stacked Column Chart",
+                plotlyOptions: {width: 900, height: 500}
+            }
+        });
+    }
+
+    onDrop(files) {
+        this.setState({
+            mode: "thinking",
+            filename: "(Parsing CSV...)",
+            textarea: "",
+            pivotState: { data: [] }
+        }, () => Papa.parse(files[0], {
+            skipEmptyLines: true,
+            error: (e) => alert(e),
+            complete: (parsed) => this.setState({
+                mode: "file",
+                filename: files[0].name,
+                pivotState: { data: parsed.data }
+            }) })
+        );
+    }
+
+    onType(event) {
+        Papa.parse(event.target.value, {
+            skipEmptyLines: true,
+            error: (e) => alert(e),
+            complete: (parsed) => this.setState({
+                mode: "text",
+                filename: "Data from <textarea>",
+                textarea: event.target.value,
+                pivotState: { data: parsed.data }
+            })
+        });
+    }
+
+    render() {
+        return (<div>
+            <div className="row text-center">
+                <div className="col-md-3 col-md-offset-3">
+                <p>Try it right now on a file...</p>
+                <Dropzone onDrop={this.onDrop.bind(this)} accept="text/csv" className="dropzone"
+                activeClassName="dropzoneActive" rejectClassName="dropzoneReject" >
+                    <p>Drop a CSV file here, or click to choose a file from your computer.</p>
+                </Dropzone>
+                </div>
+                <div className="col-md-3 text-center">
+                <p>...or type or paste some data:</p>
+                <textarea value={this.state.textarea} onChange={this.onType.bind(this)}
+                    placeholder="CSV or similary-delimited data accepted"/>
+                </div>
+            </div>
+            <div className="row text-center">
+                <p><em>note: the data never leaves your browser!</em></p>
+                <br />
+            </div>
+            <div className="row">
+                <h2 className="text-center">{this.state.filename}</h2>
+                <br />
+
+                <PivotTableUISmartWrapper {...this.state.pivotState} />
+            </div>
+        </div>);
     }
 }
